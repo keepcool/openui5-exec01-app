3ec2e4d485e4d668a5e5d1ec4ee463d592fd4426
keepcool <perl@163.com>
2019-09-04 11:50:42 +0800

    mock

--- webapp/Component.js
+++ webapp/Component.js
@@ -2,10 +2,8 @@ sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/Device",
 	"com/sap/ieg/cas/msdc/Exec01/model/models",
-	"sap/ui/model/json/JSONModel",
 	"sap/ui/model/resource/ResourceModel",
-	"./controller/HelloDialog"
-], function (UIComponent, Device, models, JSONModel, ResourceModel, HelloDialog) {
+], function (UIComponent, Device, models, ResourceModel) {
 	"use strict";
 
 	return UIComponent.extend("com.sap.ieg.cas.msdc.Exec01.Component", {
@@ -28,29 +26,11 @@ sap.ui.define([
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
 			
-			//
-			//Set data model on view
-			var oData = {
-				recipient : {
-					name : "World"
-				}
-			};
-			var oModel = new JSONModel(oData);
-			this.setModel(oModel);
-
 			var i18nModel = new ResourceModel({
 				bundleName: "com.sap.ieg.cas.msdc.Exec01.i18n.i18n"
 			});
 			this.setModel(i18nModel, "i18n");
-			this._helloDialog = new HelloDialog(this.getRootControl());
 			this.getRouter().initialize();
 		},
-		exit : function(){
-			this._helloDialog.destroy();
-			delete this._helloDialog;
-		},
-		openHelloDialog : function(){
-			this._helloDialog.open();
-		}
 	});
 });
